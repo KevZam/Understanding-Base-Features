5313cbfea86fbf08353d9c3262cb73b52e30ae6f
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-03 10:41:20 -0800

    Creating a functional Component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
-    //   return (
-    //     <div className="App">
-    //       <h1>Hi, I'm a React App</h1>
-    //     </div>
-    //   );
-
-    // Create Element's first element can be a div, or your own react component.
-    // The second arguement would be a javascript object that configures it,
-    // the third argument would be the children or whatever is nested within this element.
-    return React.createElement(
-      "div",
-      null,
-      React.createElement("h1", { className: "App" }, "Hi, I'm a React App!!!")
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person />
+      </div>
     );
   }
 }
