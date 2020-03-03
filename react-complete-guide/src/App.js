4967b3ecfba73c14c01fc2d45b951b4684423021
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-03 15:29:38 -0800

    Styling

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -35,11 +35,23 @@ class App extends Component {
   };
 
   render() {
+    // In-line styling
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler("Maximillian!!")}>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler("Maximillian!!")}
+        >
           Switch Name
         </button>
         <Person
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Person.css";
 
 const person = props => {
   return (
     // The props.children accesses whatever elements or text are inside each component in between the
     // opening and closing tags.
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
