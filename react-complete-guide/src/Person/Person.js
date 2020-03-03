1f38325f0d2972909d0d337fc46684bf850dccde
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-03 11:19:50 -0800

    Working with State

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const person = props => {
   return (
-    // You can't have classes inside this element, but you can run functions that might return more
-    // dynamic data
-    <p>
-      I'm {props.name} and I am {props.age} years old!
-    </p>
+    // The props.children accesses whatever elements or text are inside each component in between the
+    // opening and closing tags.
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
