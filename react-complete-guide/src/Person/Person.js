dd43f6c8a929fbb0f0f1f71d64f909ec8113d6cb
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-03 10:59:11 -0800

    Working with Props

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const person = () => {
-  return <p>I'm a Person!</p>;
+const person = props => {
+  return (
+    // You can't have classes inside this element, but you can run functions that might return more
+    // dynamic data
+    <p>
+      I'm {props.name} and I am {props.age} years old!
+    </p>
+  );
 };
 
 export default person;
