909ac013dc316d216a806b5e2ea44af6b1e75d92
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-03 10:29:26 -0800

    JSX

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,10 +3,19 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
+    //   return (
+    //     <div className="App">
+    //       <h1>Hi, I'm a React App</h1>
+    //     </div>
+    //   );
+
+    // Create Element's first element can be a div, or your own react component.
+    // The second arguement would be a javascript object that configures it,
+    // the third argument would be the children or whatever is nested within this element.
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("h1", { className: "App" }, "Hi, I'm a React App!!!")
     );
   }
 }
