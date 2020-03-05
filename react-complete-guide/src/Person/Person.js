c1aac0a0e3642a12e6477929b54b597bee02db36
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-05 11:10:18 -0800

    Using media queries with Radium

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,10 +3,15 @@ import "./Person.css";
 import Radium from "radium";
 
 const person = props => {
+  const style = {
+    "@media (min-width: 500px)": {
+      width: "450px"
+    }
+  };
   return (
     // The props.children accesses whatever elements or text are inside each component in between the
     // opening and closing tags.
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
