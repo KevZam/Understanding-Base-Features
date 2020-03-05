88ff549c38b8eaad8aed3e5829a36f4616b33939
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-05 11:56:35 -0800

    finishing styled components library

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,14 +4,14 @@ import Person from "./Person/Person";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => (props.alt ? "red" : "green")};
   font: inherit;
   border: 1px solid blue;
   padding: 8px;
   cursor: pointer;
 
   &:hover {
-    background-color: lightgreen;
+    background-color: ${props => (props.alt ? "salmon" : "lightgreen")};
     color: black;
   }
 `;
@@ -119,7 +119,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton onClick={this.togglePersonsHandler}>
+        <StyledButton
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
           Toggle Persons
         </StyledButton>
         {persons}
