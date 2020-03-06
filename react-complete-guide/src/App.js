3ed08f3f61daf3fe2c5f08424d083aac858e7463
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-03-05 17:21:05 -0800

    css modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const StyledButton = styled.button`
-  background-color: ${props => (props.alt ? "red" : "green")};
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
+// const StyledButton = styled.button`
+//   background-color: ${props => (props.alt ? "red" : "green")};
+//   font: inherit;
+//   border: 1px solid blue;
+//   padding: 8px;
+//   cursor: pointer;
 
-  &:hover {
-    background-color: ${props => (props.alt ? "salmon" : "lightgreen")};
-    color: black;
-  }
-`;
+//   &:hover {
+//     background-color: ${props => (props.alt ? "salmon" : "lightgreen")};
+//     color: black;
+//   }
+// `;
 
 class App extends Component {
   state = {
@@ -69,20 +69,8 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-      // using radium to create a hover property inline
-      ":hover": {
-        backgroundColor: "lightgreen",
-        color: "black"
-      }
-    };
-
     let persons = null;
+    let btnClass = [classes.button];
 
     if (this.state.showPersons) {
       persons = (
@@ -101,30 +89,28 @@ class App extends Component {
           })}
         </div>
       );
-      style.backgroundColor = "red";
-      style[":hover"] = {
-        backgroundColor: "salmon",
-        color: "black"
-      };
+
+      btnClass.push(classes.Red);
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length < 3) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length < 2) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button
           alt={this.state.showPersons}
           onClick={this.togglePersonsHandler}
+          className={btnClass.join(" ")}
         >
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,33 +1,19 @@
 import React from "react";
 // import "./Person.css";
-import styled from "styled-components";
+import classes from "./Person.css";
 
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
 const person = props => {
   const style = {};
   return (
     // The props.children accesses whatever elements or text are inside each component in between the
     // opening and closing tags.
-    <StyledDiv>
-      <div className="Person" style={style}>
-        <p onClick={props.click}>
-          I'm {props.name} and I am {props.age} years old!
-        </p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name} />
-      </div>
-    </StyledDiv>
+    <div className={classes.Person} style={style}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
   );
 };
 
